cb8d4bdc816065006b287070c243b6aebdc6f909
Jacobo Soffer <sofferjacob@gmail.com>
2022-04-27 17:06:50 -0500

    add grid layouts

--- grid/index.js
+++ grid/index.js
@@ -9,8 +9,13 @@ for (let i = 0; i < grids.length; i++) {
     }
 }
 
-const test = document.getElementById("test");
-test.onclick = (e) => {
-    console.log("click")
-    window.open(`${i + 1}/grid.html`)
+const links = document.getElementById('links');
+
+for (let i = 1; i != 9; i++) {
+    const element = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = `${i}/grid.html`;
+    link.innerText = `Grid ${i}`
+    element.appendChild(link);
+    links.appendChild(element);
 }
